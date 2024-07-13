7b7f5f93a834a37c780e810407566aae8509d95f
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-07-13 13:04:01 +0500

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Bakhrom Usmanov on 13/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
